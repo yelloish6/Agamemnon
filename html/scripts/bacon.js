87209f09e45395c6a1e82706a64446110fdc4c11
bogdan <urs.bogdan.miron@gmail.com>
2016-03-27 19:06:39 +0300

    updates on displaying input status as the color of the button - still not working

--- html/scripts/bacon.js
+++ html/scripts/bacon.js
@@ -19,8 +19,9 @@
 
 			webiopi().refreshGPIO(true);
         		
-	       		var content, button;
+	       		var content, button, buttontest;
         		content = $("#content");
+					$(refreshButtons);
         		
 			button = webiopi().createGPIOButton(22,"Back-up control");
 			content.append(button);        		
@@ -31,7 +32,7 @@
         		button = webiopi().createSequenceButton("dining", "Dining", 18, 200, "01");
         		content.append(button);
         		
-        		button = webiopi().createSequenceButton("living", "Living", 22, 200, "01");
+			button = webiopi().createSequenceButton("living", "Living", 22, 200, "01");
 			var state = webiopi().digitalRead(27, gpioReadCallback);
 			function gpioReadCallback(gpio, value){
 				if (value == 1) {
@@ -40,8 +41,30 @@
 						webiopi().setClass("button", "LOW");
 					}	
 			}
-        		content.append(button);
+	
+			content.append(button);
 			
+
+     			//create test button to test style change
+			buttontest = w().createButton("testbutton", "Test Button");
+			buttontest.bind(BUTTON_DOWN, function(event) {
+				w().outputSequence(22, 200, "01");
+			});
+			content.append(buttontest);
+						
+			function refreshButtons() {
+				if (w().digitalRead(27) == 1) {
+						$("#testbutton").html('<style type="text/css">button#testbutton { background-color: red;}</style><span style="color:white">Test Button</span>');
+					} else {
+						$("#testbutton").html('<style type="text/css">button#testbutton { background-color: black;}</style><span style="color:white">Test Button</span>');
+				}
+			}
+			setTimeout(refreshButtons, 1000);
+			
+
+			//end test area
+
+
 			button = webiopi().createGPIOButton(27, "LivingStat");
 			content.append(button);
         		
@@ -76,3 +99,5 @@
         		
         		
         });
+
+
