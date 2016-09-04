ab50e9fb2de25a8ea2e84641e954a1741b3b6189
bogdan <urs.bogdan.miron@gmail.com>
2016-09-04 23:25:51 +0300

    Connected all 8 relays to the RasPI board and modified bacon.js to match the connections. Unused buttons are commented out

--- html/scripts/bacon.js
+++ html/scripts/bacon.js
@@ -1,14 +1,14 @@
  webiopi().ready(function() {
-	 			webiopi().setFunction(2,"out");
-	 			webiopi().setFunction(3,"out");
+//	 			webiopi().setFunction(2,"out");
+//	 			webiopi().setFunction(3,"out");
 	 			webiopi().setFunction(4,"out");
-	 			webiopi().setFunction(7,"out");
-	 			webiopi().setFunction(8,"out");
-	 			webiopi().setFunction(9,"out");
+//	 			webiopi().setFunction(7,"out");
+//	 			webiopi().setFunction(8,"out");
+//	 			webiopi().setFunction(9,"out");
 	 			webiopi().setFunction(10,"out");
-	 			webiopi().setFunction(11,"out");
-	 			webiopi().setFunction(14,"out");
-	 			webiopi().setFunction(15,"out");
+//	 			webiopi().setFunction(11,"out");
+//	 			webiopi().setFunction(14,"out");
+//	 			webiopi().setFunction(15,"out");
 	 			webiopi().setFunction(17,"out");
         		webiopi().setFunction(18,"out");
         		webiopi().setFunction(22,"out");
@@ -26,13 +26,13 @@
 			button = webiopi().createGPIOButton(22,"Back-up control");
 			content.append(button);        		
 
-			button = webiopi().createSequenceButton("bucatarie", "Bucatarie", 17, 200, "01");
+			button = webiopi().createSequenceButton("bucatarie", "Bucatarie", 4, 200, "01");
         		content.append(button);
         		
-        		button = webiopi().createSequenceButton("dining", "Dining", 18, 200, "01");
+        		button = webiopi().createSequenceButton("dining", "Dining", 10, 200, "01");
         		content.append(button);
         		
-			button = webiopi().createSequenceButton("living", "Living", 22, 200, "01");
+			button = webiopi().createSequenceButton("living", "Living", 17, 200, "01");
 			var state = webiopi().digitalRead(27, gpioReadCallback);
 			function gpioReadCallback(gpio, value){
 				if (value == 1) {
@@ -68,32 +68,32 @@
 			button = webiopi().createGPIOButton(27, "LivingStat");
 			content.append(button);
         		
-        		button = webiopi().createSequenceButton("baie", "Baie", 23, 200, "01");
+        		button = webiopi().createSequenceButton("baie", "Baie", 18, 200, "01");
         		content.append(button);
         		
-        		button = webiopi().createSequenceButton("baie", "Baie Spot", 23, 200, "01");
-        		content.append(button);
+//        		button = webiopi().createSequenceButton("baie", "Baie Spot", 23, 200, "01");
+//        		content.append(button);
         		
-        		button = webiopi().createSequenceButton("hol", "Hol", 24, 200, "01");
+        		button = webiopi().createSequenceButton("hol", "Hol", 22, 200, "01");
         		content.append(button);
 
-        		button = webiopi().createSequenceButton("garaj", "Garaj", 22, 200, "01");
+        		button = webiopi().createSequenceButton("garaj", "Garaj", 23, 200, "01");
         		content.append(button);
         		
-        		button = webiopi().createSequenceButton("camara", "Camara", 22, 200, "01");
+//        		button = webiopi().createSequenceButton("camara", "Camara", 22, 200, "01");
+//        		content.append(button);
+        		
+        		button = webiopi().createSequenceButton("camera_tehnica", "C. Tehnica", 24, 200, "01");
         		content.append(button);
         		
-        		button = webiopi().createSequenceButton("camera_tehnica", "C. Tehnica", 22, 200, "01");
+        		button = webiopi().createSequenceButton("pult", "Pult", 25, 200, "01");
         		content.append(button);
         		
-        		button = webiopi().createSequenceButton("pult", "Pult", 22, 200, "010");
-        		content.append(button);
+//       		button = webiopi().createSequenceButton("ext_camera_tehnica", "Ext C.T.", 22, 200, "01");
+//       		content.append(button);
         		
-        		button = webiopi().createSequenceButton("ext_camera_tehnica", "Ext C.T.", 22, 200, "01");
-        		content.append(button);
-        		
-        		button = webiopi().createSequenceButton("ext_terasa", "Ext Terasa", 22, 200, "01");
-        		content.append(button);
+//        		button = webiopi().createSequenceButton("ext_terasa", "Ext Terasa", 22, 200, "01");
+//        		content.append(button);
         		
 			
         		
